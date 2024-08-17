3fd991f30f554da8ca62b7db4ba44d859b56c624
Belden Schroeder <beldenschroeder@gmail.com>
2024-08-16 17:27:18 -0700

    Updating bucket resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,28 +18,36 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_s3_bucket" "s3Bucket" {
-     bucket = "terraform-with-github-actions-website"
-     acl       = "public-read"
+# resource "aws_s3_bucket" "s3Bucket" {
+#      bucket = "terraform-with-github-actions-website"
+#      acl       = "public-read"
 
-     policy  = <<EOF
-{
-  "id" : "MakePublic",
-  "version" : "2012-10-17",
-  "statement" : [
-    {
-      "action" : [
-          "s3:GetObject"
-      ],
-      "effect" : "Allow",
-      "resource" : "arn:aws:s3:::terraform-with-github-actions-website/*",
-      "principal" : "*"
-      }
-    ]
-  }
-EOF
+#      policy  = <<EOF
+# {
+#   "id" : "MakePublic",
+#   "version" : "2012-10-17",
+#   "statement" : [
+#     {
+#       "action" : [
+#           "s3:GetObject"
+#       ],
+#       "effect" : "Allow",
+#       "resource" : "arn:aws:s3:::terraform-with-github-actions-website/*",
+#       "principal" : "*"
+#       }
+#     ]
+#   }
+# EOF
 
-  website {
-    index_document = "index.html"
+#   website {
+#     index_document = "index.html"
+#   }
+# }
+
+resource "aws_s3_bucket_website_configuration" "s3Bucket" {
+  bucket = "terraform-with-github-actions-website"
+
+  index_document {
+    suffix = "index.html"
   }
 }
